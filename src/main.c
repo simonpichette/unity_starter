4aace6b4b5ed82d91e377d07da411569a1fd2179
Simon Pichette <virtualsim@gmail.com>
2020-05-26 17:42:17 -0400

    Reset main.c

--- src/main.c
+++ src/main.c
@@ -1,28 +1,6 @@
-#include "../test/unity/unity.h"
+#include <stdio.h>
 
-void setUp(void)
-{
-    // set stuff up
-}
-
-void tearDown(void)
-{
-    // clean up when done
-}
-
-void test1(void)
-{
-
-}
-
-void test2(void)
-{
-
-}
 
 int main() {
-    UNITY_BEGIN();
-    RUN_TEST(test1);
-    RUN_TEST(test2);
-    return UNITY_END();
+    printf("***\nSalut, le monde!\n***\n");
 }
